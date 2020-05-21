3cb00f488aafc4c24ed0bf4f6fbb22431db07a82
Marcos Koch <marcos_koch@hotmail.com>
2020-05-21 15:29:55 -0300

    feat: config react-router-dom

---
+++ frontend/src/pages/Apartment/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Apartment() {
+  return <div />;
+}
+
+export default Apartment;
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Apartment from '../pages/Apartment';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/apartment" component={Apartment} isPrivate />
+    </Switch>
+  );
+}
